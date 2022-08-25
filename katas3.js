1fe25d353f6ff10cb783ca5a9ccb82f01dffbd11
Ana Carolina Oliveira <carol.oliva.cruz@gmail.com>
2022-08-25 10:30:06 -0300

    add

--- katas3.js
+++ katas3.js
@@ -1,40 +1,201 @@
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 17, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
 function kata1() {
-    // implemente o código do kata 1 aqui
+
+    let listNum = [];
+
+    for (let i = 1; i <= 25; i++) {
+
+        listNum.push(i)
+    }
+    return listNum;
+
 }
+console.log(kata1());
+
+
+//
 
 function kata2() {
-    // implemente o código do kata 2 aqui
+
+    let listNum = [];
+
+    for (let i = 25; i >= 1; i--) {
+        listNum.push(i);
+    }
+    return listNum;
+
+} console.log(kata2())
+
+
+//
+
+function positivoForNegativo(num) {
+
+    let multiplicacao = num - (num * 2);
+
+    return multiplicacao;
 }
+
+
+
+//
 
 function kata3() {
-    // implemente o código do kata 3 aqui
+
+    let listNum = [];
+
+    for (let i = 1; i <= 25; i++) {
+        let negativo = positivoForNegativo(i);
+        listNum.push(negativo);
+    }
+    return listNum;
 }
+console.log(kata3());
+
+
+
+
+//
 
 function kata4() {
-    // implemente o código do kata 4 aqui
-}
+    let listNum = [];
+    for (let i = 25; i >= 1; i--) {
+        let negativo = positivoForNegativo(i)
+        listNum.push(negativo)
+    }
+    return listNum
+
+} console.log(kata4())
+
+
+
+
+//
 
 function kata5() {
-    // implemente o código do kata 5 aqui
+    let listNum = [];
+    let listaImpar = [];
+
+    for (let i = 25; i >= 1; i--) {
+        let negativo = positivoForNegativo(i)
+        listNum.push(negativo)
+    }
+
+    for (let i = 1; i <= 25; i++) {
+
+        listNum.push(i)
+    }
+
+    if (listNum % 2 === 1) {
+        listaImpar.push(listNum)
+    }
+    return listaImpar;
+
 }
+console.log(kata5())
+
+
+
+
+//
 
 function kata6() {
-    // implemente o código do kata 6 aqui
+
+    let listaDivisivel3 = [];
+
+    for (let i = 0; i <= 100; i++) {
+        let numAtual = i;
+        if (numAtual % 3 === 0) {
+            listaDivisivel3.push(numAtual);
+        }
+    }
+    return listaDivisivel3
 }
+console.log(kata6());
+
+
+
+
+//
 
 function kata7() {
-    // implemente o código do kata 7 aqui
+
+    let listaDivisivel7 = [];
+
+    for (let i = 0; i <= 100; i++) {
+        let numAtual = i;
+        if (numAtual % 7 === 0) {
+            listaDivisivel7.push(numAtual);
+        }
+    }
+    return listaDivisivel7;
 }
+console.log(kata7());
+
+
+
+
+//
 
 function kata8() {
-    // implemente o código do kata 8 aqui
-}
+    let divisiveis3E7 = [];
 
-function kata9() {
-    // implemente o código do kata 9 aqui
+    for (let i = 100; i > 1; i--) {
+        let numAtual = i;
+        if (numAtual % 3 === 0) {
+            divisiveis3E7.push(numAtual);
+        }
+    }
+
+    for (let i = 100; i > 1; i--) {
+        let numAtual = i;
+        if (numAtual % 7 === 0) {
+            divisiveis3E7.push(numAtual);
+        }
+    }
+
+    return divisiveis3E7;
 }
+console.log(kata8());
+
+
+
+//
+
+let divisivel5 = [];
+
+function listaDivisiveis5(){
+
+    for (let i = 1; i <= 100; i++) {
+        let numAtual = i;
+        if (numAtual % 5 === 0) {
+            divisivel5.push(numAtual);
+        }
+    }
+    return divisivel5;
+}
+console.log(listaDivisiveis5());
+
+
+function kata9(lista) {
+    
+    let divisivel5Impar = [];
+    
+    for (let i = 0; i < lista.length; i++) {
+
+        let numNovo = lista[i];
+
+        if (numNovo % 2 !== 0) {
+            divisivel5Impar.push(numNovo);
+        }
+    }
+    return divisivel5Impar;
+ }
+ console.log(kata9(divisivel5));
+
+
+
 
 function kata10() {
     // implemente o código do kata 10 aqui
